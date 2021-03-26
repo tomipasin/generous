cdb52b4cbda828d2a476f7e5f022a247a30aa995
tomipasin <tomipasin@gmail.com>
2021-03-26 16:53:31 -0300

    v2.4 - IMage bug solved

--- TLMtoBC.js
+++ TLMtoBC.js
@@ -4,6 +4,7 @@ let parser2JSON = require('xml2json');
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
 let http = require("https");
 let request = require("request");
+const { number } = require('yargs');
 require('dotenv').config({ path: '.env' })
 fs = require('fs');
 
@@ -17,7 +18,9 @@ function TLMdataToBC(arg) {
         res.on('end', function () {
             parser.parseString(data, function (error, result) {
                 if (error === null) {
-                    console.log(data)
+                    var opt = {
+                        arrayNotation: ['photos', '/photos'],
+                    };
                     let json = parser2JSON.toJson(data);
                     console.log((json))
                     createProductSchema(json)
@@ -50,6 +53,7 @@ function TLMdataToBC(arg) {
                     priceOK = parseFloat(priceConv.toString().replace(/\./g, ',').replace(',', '.'))
                 }
             }
+            let cat = jsonData[key].artikelgroep
             let category = jsonData[key].artikelgroep_id
             if (category != 23) {
                 category = [23]
@@ -64,15 +68,16 @@ function TLMdataToBC(arg) {
             } else {
                 weight = 0
             }
+
             let imagesOK = []
             let images = jsonData[key].photos.photo
             if (images) {
-                console.log(key)
                 for (const chave in images) {
                     let imgOK = `${images[chave]}`
                     const img = { image_url: imgOK, is_thumbnail: true }
                     if (imgOK != '[object Object]' && imgOK != '1') {
                         if (img != '{ image_url: "1", is_thumbnail: true }') {
+                            //console.log(img)
                             imagesOK.push(img)
                         }
                     }
@@ -82,13 +87,13 @@ function TLMdataToBC(arg) {
                         const mto = images[key].$t
                         const img = { image_url: mto, is_thumbnail: true }
                         if (mto != undefined) {
+                            //console.log(img)
                             imagesOK.push(img)
 
                         }
                     }
                 })
             }
-            
             //here's the schema...
             const toUpdate = {
                 data: {
@@ -104,14 +109,17 @@ function TLMdataToBC(arg) {
                     images: imagesOK
                 }
             }
-            //all set so let's create the products in BC store.
             console.log(toUpdate.data)
+
+            //all set so let's create the products in BC store.
             createProduct(toUpdate.data)
+
         })
     }
-    //Step 3 - create or update the products in BC store.
+
+    //The function to create...
     function createProduct(data) {
-        let options = {
+        var options = {
             method: 'POST',
             url: `https://api.bigcommerce.com/stores/${process.env.STORE}/v3/catalog/products`,
             headers: {
@@ -122,60 +130,67 @@ function TLMdataToBC(arg) {
             body: data,
             json: true
         };
-        //Using request I can check the server's response. If is OK the product is already created on store.
-        //If has no errors the code stop here but if I have a conflict response (code 409) it means that the 
-        //product already exists and it need to be updated, so the code go ahead with this.
+        //Using request I can check if the server's response are OK the products are created on store, 
+        //no errors and the code finish here. 
+        //But if I have a conflict response (code 409) it means that the product already exists and
+        //we need proceed the update.
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
             console.log(response.statusCode)
             if (response.statusCode === 409) {
                 getDataFromBCBySKU(data.sku, data)
             } else {
+                console.log(response.statusCode)
                 return;
             }
         });
+
+        //Ahd here's my function to get BC data with IDs using the SKU as a kind of indexer. 
+        function getDataFromBCBySKU(sku, data) {
+            let options = {
+                method: 'GET',
+                url: `https://api.bigcommerce.com/stores/${process.env.STORE}/v2/products?sku=${sku}`,
+                headers: { 'x-auth-token': `${process.env.TOKEN}` }
+            };
+            request(options, function (error, _response, body) {
+                if (error) throw new Error(error);
+                let xml = body;
+                let json = JSON.parse(parser2JSON.toJson(xml));
+                let BC_id = json.products.product.id;
+                console.log(json)
+                //with BC Ids I call the function thar properly get the data and update the BC store. 
+                updateBCfromTLM(BC_id, json)
+            });
+        }
+        //this is a simples PUT function...
+        function updateBCfromTLM(id, data) {
+            let options = {
+                method: 'PUT',
+                url: `https://api.bigcommerce.com/stores/${process.env.STORE}/v3/catalog/products/${id}`,
+                headers: {
+                    accept: 'application/json',
+                    'content-type': 'application/json',
+                    'x-auth-token': `${process.env.TOKEN}`
+                },
+                body: data,
+                json: true
+            };
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                console.log(body)
+            });
+        }
     }
-    //Here's my function to get BC data with IDs using the SKU as a kind of indexer. 
-    function getDataFromBCBySKU(sku, data) {
-        const skuNumber = sku;
-        let options = {
-            method: 'GET',
-            url: `https://api.bigcommerce.com/stores/${process.env.STORE}/v2/products?sku=${skuNumber}`,
-            headers: { 'x-auth-token': `${process.env.TOKEN}` }
-        };
-        request(options, function (error, _response, body) {
-            if (error) throw new Error(error);
-            let xml = body;
-            let json = JSON.parse(parser2JSON.toJson(xml));
-            let BC_id = json.products.product.id;
-            console.log(json)
-            //with BC Ids I call the function thar properly get the data and update the BC store. 
-            updateBCfromTLM(BC_id, data)
-        });
-    }
-    //and the PUT function:
-    function updateBCfromTLM(id, data) {
-        let options = {
-            method: 'PUT',
-            url: `https://api.bigcommerce.com/stores/${process.env.STORE}/v3/catalog/products/${id}`,
-            headers: {
-                accept: 'application/json',
-                'content-type': 'application/json',
-                'x-auth-token': `${process.env.TOKEN}`
-            },
-            body: data,
-            json: true
-        };
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-            console.log(body)
-        });
-    }
+
 }
 //to this "update" from customer's store occurs every 24h (86400 seconds) please
 //uncoment the line below.
+
 
 //setTimeout(TLMdataToBC, 86400000);
 
 //and comment this one... ;-)
 TLMdataToBC();
+
+
+
